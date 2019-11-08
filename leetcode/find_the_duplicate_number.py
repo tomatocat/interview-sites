5dc3d169ca2e2f7fbfe04fc58395df0aeb0f2ecf
Jeremy Wang <44682631+tomatocat@users.noreply.github.com>
2019-11-08 17:41:22 -0500

    Create find_the_duplicate_number.py

---
+++ leetcode/find_the_duplicate_number.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/find-the-duplicate-number/
+
+class Solution(object):
+    def findDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        for i in range(len(nums)):
+            if nums[i] == nums[i+1]:
+                return nums[i]
